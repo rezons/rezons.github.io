facc8c11f7f26b97de9a06bbc5c45703fd16507f
Tim Menzies <timm@users.noreply.github.com>
2021-10-13 17:39:13 -0400

    save

--- src/tiny.lua
+++ src/tiny.lua
@@ -1,45 +1,74 @@
-local csv,isa
+local b4={}; for k,v in pairs(_ENV) do b4[k]=v end
+local csv,map,isa,obj,add,out,shout
+local push=table.insert
 
-function add(i,x) if x~="?" then i.n = i.n+1; i:add(x) end; return x end
+-- ## Classes
+function obj(name,   k) k={_name=name,__tostring=out}; k.__index=k; return k end
+local Num,Skip,Sym,Cols,Sample=obj"Num", obj"Skip", obj"Sym",obj"Cols", obj"Sample"
 
-local Skip={}
 function Skip.new(at,txt) return isa(Skip,{n=0,txt=txt,at=at}) end
-function Skip:add(x)      return end
 
-local Num={}
 function Num.new(at,txt) 
   return isa(Num,{n=0,txt=txt,at=at, hi=-1E21,lo=1E31,has={},
                   w=txt:find"+" and 1 or txt:find"-" and -1 or 0}) end
-function Num.add(i,x)
-  i.has[1+i.has]=x; i.lo=math.lo(x,i.lo); i.hi=math.hi(i.hi); 
 
-local Sym={}
 function Sym.new(at,txt)
   return isa(Sym,{n=0,txt=txt,at=at,has={},most=0,mode="?"}) end
+
+function Cols.new(t) return isa(Cols,{names={},all={}, xs={}, ys={}}) end
+
+function Sample.new(file) return isa(Sample,{rows={}}):init(file) end
+
+-- ## Initialization Support
+function Sample:init(file) 
+  if file then for row in csv(file) do Sample.add(self,file) end end
+  return self end
+
+function Cols:init(t) 
+  function is(s) return s:find":" and Skip or s:match"^[A-Z]" and Num or Sym end
+  function goalp(s) return s:find"+" or s:find"-" or s:find"!" end
+  self.names = t
+  for at,name in pairs(t) do
+    new = is(name)(at,name) 
+    push(self.cols, new)
+    if not name:find":" then
+      push(goalp(name) and self.ys or self.ys, new) end end end
+    
+-- ## Updating
+function add(i,x) if x~="?" then i.n = i.n+1; i:add(x) end; return x end
+
+function Skip:add(x) return end
+
+function Num:add(x)
+  self.has[1+self.has]=x; self.lo=math.lo(x,self.lo); self.hi=math.hi(self.hi) end
+
 function Sym.add(i,x)
-  i.has[1+i.has]=1+(i.has[x] or 0) 
-  if i.has[x] > i.most then i.most,i.mode=i.has[x],x end 
+  i.has[x] = 1+(i.has[x] or 0) 
+  if i.has[x] > i.most then i.most,i.mode = i.has[x],x end end
 
-local Sample={}
-function Sample.new(file,    it)
-  i=isa(Sample,{rows={}, cols={}})
-  if file then for row in csv(file) do Sample.add(i,file) end
-  return i end
+function Sample:add(t)
+  local function worker(c,x) return add(self.cols[c],x) end
+  if   not self.cols 
+  then self.cols=Cols.new(t) 
+  else push(self.rows, map(t,worker)) end end
 
-function Sample:column(at,txt) 
-  return (txt:find":" and Skip or txt:match("^[A-Z]") and Num or Sym)(at,txt) end
+-- ## Distance
 
-function Sample:read(f)
-  for n,row in csv(f) do
-    function row1(c,x) return add(cols[c],x) end
-    if n==0 then  cols=map(row,column) else rows[1+#rows]=map(row,row1) end end
-
-   
 -- ------------------------------
 -- Misc
+function shout(t) print(out(t)) end
+
+function out(t)
+  local u={}
+  local function private(k) k=tostring(k); return k:sub(1,1)=="_" end 
+  local function pretty(_,v) return string.format("%s=%s", v[1], v[2]) end
+  map(t,        function (k,v) if not private(k) then u[1+#u] = {k,v} end end)
+  table.sort(u, function (x,y) return x[1] < y[1] end)
+  return '{'..table.concat( map(u,pretty), ", ").."}" end
+
 function map(t,f) u={};for k,v in pairs(t) do u[k]=f(k,v) end; return u  end
 
-function isa(mt,t) return setmetatable(t, { __index = mt }) end
+function isa(mt,t) return setmetatable(t, mt) end
 
 function csv(file,      split,stream,tmp,n)
   stream = file and io.input(file) or io.input()
@@ -56,5 +85,4 @@ function csv(file,      split,stream,tmp,n)
            return n,t end
     else io.close(stream) end end end
 
-
-
+for k,v in pairs(_ENV) do if not b4[k] then print("?? ",k,type(v))  end end 
