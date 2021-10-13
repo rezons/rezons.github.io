edcc1a3bc6d6079442fd54ab62a07b6476559810
Tim Menzies <timm@ieee.org>
2021-10-13 10:35:52 -0400

    save

--- test/ok.lua
+++ test/ok.lua
@@ -1,8 +1,9 @@
 package.path = '../src/?.lua'
 local b4={}; for k,v in pairs(_ENV) do b4[k]=v end
 
-local function bad(...) print('\27[1m\27[31m'..string.format(...)..'\27[0m') end
-local function ok(...)  print('\27[1m\27[32m'..string.format(...)..'\27[0m') end
+local red,green
+function red(...) print('\27[1m\27[31m'..string.format(...)..'\27[0m') end
+function green(...)  print('\27[1m\27[32m'..string.format(...)..'\27[0m') end
 
 local fails=0
 
@@ -10,8 +11,8 @@ for _,f in ipairs(arg) do
   if f ~= "ok.lua" and f ~= "lua" then
     local ok,msg = pcall(dofile,f) 
     if   ok 
-    then ok("%s",f)
-    else bad("%s",tostring(msg)); fails=fails+1 end end end 
+    then green("%s",f)
+    else red("%s",tostring(msg)); fails=fails+1 end end end 
 
 for k,v in pairs(_ENV) do if not b4[k] then print("?? ",k,type(v))  end end 
 print(2)
