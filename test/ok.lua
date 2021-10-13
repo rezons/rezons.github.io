0ffb57ce5643c4fd7a49e0c001a50abc325b958f
Tim Menzies <timm@ieee.org>
2021-10-12 23:37:29 -0400

    save

--- test/ok.lua
+++ test/ok.lua
@@ -12,8 +12,8 @@ for _,f in pairs(arg) do
   if f ~= "ok.lua" and f ~= "lua" then
     local ok,msg = pcall(function () dofile(f) end) 
     if   ok 
-    then green("✔ %s",f)
-    else red("✘ %s",tostring(msg)); fails=fails+1 end end end 
+    then green(" %s",f)
+    else red(" %s",tostring(msg)); fails=fails+1 end end end 
 
 for k,v in pairs(_ENV) do 
   if not b4[k] then print("?? ",k,type(v))  end end 
