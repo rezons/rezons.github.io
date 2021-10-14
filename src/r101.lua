d73fd6dc570480a34297f5c7fd72369df7cef0f2
Tim Menzies <timm@users.noreply.github.com>
2021-10-14 11:25:39 -0400

    save

--- src/r101.lua
+++ src/r101.lua
@@ -5,6 +5,7 @@ local b4={}; for k,v in pairs(_ENV) do b4[k]=v end
 -- Divide data samples in two. Apply some _reasons_ over the `x` or `y` variables
 -- to favor one half. Find and print the variable range that most distinguishes one half from the over.
 -- Cull anything that contradicts that range. Repeat.
+-- <br clear=all>
 local csv,map,isa,obj,add,out,shout,str
 local push=table.insert
 
