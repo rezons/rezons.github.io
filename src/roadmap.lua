5fa398081b4a2b617bc3da564af72ccd3493a454
Tim Menzies <timm@users.noreply.github.com>
2021-10-15 13:36:53 -0400

    save

---
+++ src/roadmap.lua
@@ -0,0 +1,72 @@
+--[[
+
+- strucutres
+  1. business vieww: (fromhttp://web.eecs.umich.edu/~weimerw/2019-481F/readings/infose.pdf:#page=9
+     - When = past, present, future
+     - What = explore, analyze, experiment
+     - How = Explanation
+     - Who = Goals
+  2. systems view. y=f(x): 
+     - build f with limited access to y (aka semi-supervised learning)
+     - y can be symbolic or numeric
+     - y can be single or multi goal (and if the latter, then there are weights)
+
+divide (sort, clster)
+split numeris
+merge
+contrast
+
+explore past = trends = sort by time, divide, discretize, contrast
+analsize past = summarize =  constrast b^2/(a+b)
+experiment past = model = cluster, descrecirze contrast "max a" or "max b"
+
+explore present= cluster, index, outliers 1/(a+b)
+analuze present: asscoaition. what ahppens here but not there b^2/(a+b)
+anaylze present: sort by normalized b, sort by normalized a
+
+future forecast: b^2/(b+a)
+anaysize goals: planning it + b -a
+experiment fugure: comptuer a,b
+
+planning, optimize, monitor, tabu
+
+problem-oriented
+- what is the problem? exploring the world
+- pile of data representing thw reold explore the rod
+- unfold details.
+  - lua 101
+    - cli
+    - printing objects
+    - test cases
+  - lua objects 101
+
+- student projects: code in in python, typescript, rust
+
+- biclustering. code that is shown ad code that is runable
+
+- divide into byte size chunks
+
+- business needs
+
+- ethics , explanations
+
+- i can see a programming 
+
+hey too many knobs, too many options. people use too few. and 30% errors on config
+
+
+hwy pythin: environment hell
+
+deer in the spotlight. ai being out of control
+
+- nB
+- KNN
+- decision tree
+- MOEA deonstrat 
+
+theory lists
+
+- unfair demnstratation
+- hyperparmater demonstration
+--]]
+
