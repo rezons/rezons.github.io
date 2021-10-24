5e36e3c4435277832d13799751f0158a4536604f
Tim Menzies <timm@ieee.org>
2021-10-24 00:24:22 -0400

    save

--- src/keys.lua
+++ src/keys.lua
@@ -13,11 +13,12 @@ local the = {
     wild= cli("-W", false) }
 
 -- ## Short-cuts
-local cat,fmt,push,sort
+local cat,fmt,push,sort,f3
 cat  = table.concat
 fmt  = string.format
 push = table.insert
 sort = function(t,f) table.sort(t,f); return t end
+f3   = function(_,x) return fmt("%.3f",x) end
 
 -- ## Lists
 local keys,map
@@ -41,10 +42,9 @@ function obj(is,   o) o={_is=is,__tostring=out}; o.__index=o; return o end
 
 -- ## Files
 local csv
-function csv(file,      n,split,stream,tmp)
+function csv(file,      split,stream,tmp)
   stream = file and io.input(file) or io.input()
   tmp    = io.read()
-  n      = 0
   return function(       t)
     if   tmp 
     then t,tmp = {},tmp:gsub("[\t\r ]*",""):gsub("#.*","")
@@ -52,14 +52,13 @@ function csv(file,      n,split,stream,tmp)
          tmp = io.read()
          if  #t > 0
          then for j,x in pairs(t) do t[j] = tonumber(x) or x end
-              n=n+1
-              return n,t end
+              return t end
     else io.close(stream) end end  end
 
 local Sample=obj"Sample"
 function Sample.new(file,    self) 
-  self = isa(Sample,{_rows={}, sym={},hi={},lo={},weight={}}) 
-  if file then for n,row in csv(file) do self:add(n,row) end end
+  self = isa(Sample,{_rows={}, names={},sym={},hi={},mu={},lo={},weight={}}) 
+  if file then for row in csv(file) do self:add(row) end end
   return self end
 
 function Sample:header(k,v)
@@ -71,21 +70,30 @@ function Sample:syms(k,v)
   self.sym[k][v] = (self.sym[k][v] or 0) + 1 end
 
 function Sample:hilos(k,v) 
-  if not self.hi[k] then self.hi[k],self.lo[k]=v,v end
+  if not self.hi[k] then self.hi[k], self.lo[k],self.mu[k] = v,v,0 end
   if v > self.hi[k] then self.hi[k] = v end
-  if v < self.lo[k] then self.lo[k] = v end end
+  if v < self.lo[k] then self.lo[k] = v end 
+  local d = v - self.mu[k]
+  self.mu[k] = self.mu[k] + d/((#self._rows) + 1)
+  end
 
 function Sample:keep(k,v)
   if v=="?" then return v end
   if type(v)=="number" then self:hilos(k,v) else self:syms(k,v) end
   return v end
 
-function Sample:add(n,row)
-  if   n==1 
-  then map(row,                 function(k,v) return self:header(k,v) end) 
-  else push(self._rows, map(row, function(k,v) return self:keep(k,v)   end)) 
-  end
-end
+function Sample:add(row)
+  if   (#self.names) == 0
+  then self.names = row
+       map(row, function(k,v) return self:header(k,v) end) 
+  else push(self._rows, map(row, 
+                            function(k,v) return self:keep(k,v) end)) end end
+
+function Sample:clone(t,   s)
+  s=Sample.new()
+  s:add(self.names)
+  for _,row in pairs(t or {}) do s:add(row) end
+  return s end
 
 function Sample:norm(col,x)
   local lo,hi = self.lo[col], self.hi[col]
@@ -94,6 +102,7 @@ function Sample:norm(col,x)
 function Sample:better(row1,row2)
   local e,w,s1,s2,n,a,b,what1,what2
   local cols = self.weight
+  shout(self.weight)
   what1, what2, n, e = 0, 0, #cols, math.exp(1)
   for at,w in pairs(cols) do
     a     = self:norm(at, row1[at])
@@ -102,11 +111,19 @@ function Sample:better(row1,row2)
     what2 = what2 - e^(w * (b - a) / n) end
   return what1 / n < what2 / n end
 
+function Sample:ys(t) return map(self.weight, function(i,_) return t[i] end) end
+
 function Sample:betters()
   return sort(self._rows, function(x,y) return self:better(x,y) end) end
 
 local s=Sample.new(the.data)
-for _,row in pairs(s:betters()) do shout(row) end
+local rows=s:betters()
+for _,row  in pairs(rows) do shout(s:ys(row)) end
+-- local s1,s2=s:clone(), s:clone()
+-- for i=1,50               do s1:add(rows[i]) end
+-- for i=(#rows - 50),#rows do s2:add(rows[i]) end
+-- shout(map(s1.mu,f3))
+-- shout(map(s2.mu,f3))
 
 -- ## Start-up
 for k,v in pairs(_ENV) do if not b4[k] then print("?? ",k,type(v)) end end 
