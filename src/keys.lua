2a688ab57263627a624dbb1a7a3190d0e13261b3
Tim Menzies <timm@ieee.org>
2021-10-23 20:29:47 -0400

    save

--- src/keys.lua
+++ src/keys.lua
@@ -1,21 +1,50 @@
 local b4={}; for k,v in pairs(_ENV) do b4[k]=v end
-local Eg,lib = require"lib"
-local main = lib.main
+local lib = require"lib"
+local Eg = lib.Eg
+local csv,shout = lib.csv,lib.shout
+local the
 
 -- ## Settings, CLI
 local function keys()
-  lib.main(b4, "lua keys.lua", function() return {
-  {"bw",   "-B", false,     "show color strings in black and white"},
-  {"bins", "-b", .10,        "number of  bins"},
-  {"help", "-h", false,     "show help"},
-  {"seed", "-S", 937162211, "random number seed"},
-  {"todo", "-t", "ls",      "default start-up action"},
-  {"wild", "-W", false,     "wild mode, run actions showing stackdumps"}
-  } end) end
+  lib.main(b4, "lua keys.lua", {
+    {"bw",   "-B", false,  "show color strings in black and white"},
+    {"bins", "-b", 10,    "number of  bins"},
+    {"data", "-d", "../data/auto93.csv", "data file"},
+    {"help", "-h", false,  "show help"},
+    {"seed", "-S", 937162211, "random number seed"},
+    {"todo", "-t", "ls",  "default start-up action"},
+    {"wild", "-W", false, "wild mode, run actions showing stackdumps"}}) end
 
+local is={}
+function is.use(s) return not s:find":" end
+function is.goal(s) return s:find"+" or s:find"-" or s:find"!" end
+function is.num(s) return s:match"^[A-Z]" end
+-- -------------------------------------------------------------
+-- ## main
+Range=obj"Range"
+function Range.new() return isa(Range,{lo=1E32, hi=-1E32}) end
+function Range:add(x)
+  if x > self.hi then self.hi = x end
+  if x < self.lo then self.lo = x end end
+
+function triangle(a,c,b,   u) 
+  u = rand()
+  return (u < (c-a)/(b-a) and a + u*(b-a)*(c-a) or b - (1-u)*(b-a)*(b-c))^0.5
+
+local function main(      meta) 
+  meta=nil
+  for row in csv(the.data) do 
+    if   meta
+    then true
+    else meta={}
+         meta.uses = map(row, is.use)
+         meta.nums = map(row, function(_,s) return is.num(s) and Range.new() end)
+         meta.goals= map(row, is.goal)
+    end end end
 -- -------------------------------------------------------------
 -- ## Examples
-Eg.fred= {"asdas",function() print(1) end}
+Eg.csv= {"read from stdio", 
+         function(options) the=options; main() end}
 -- -------------------------------------------------------------
 -- ## Start-up
 keys()
--- src/lib.lua
+++ src/lib.lua
@@ -1,4 +1,4 @@
-local eg,lib={},{}
+local lib={}
 
 -- ## Classes
 -- Columns of data either `Num`eric, `Sym`bolic, or things we are going to `Skip` over.
@@ -15,19 +15,43 @@ lib.push = table.insert
 lib.sort = function(t,f) table.sort(t,f); return t end
 
 function lib.map(t,f,      u) 
-  u={};for k,v in pairs(t) do u[k]=f(k,v) end; return u end
+  f = f or function(k,v) return v end
+  u={}
+  for k,v in pairs(t) do 
+    local tmp = f(k,v)
+    if tmp then u[k] = tmp end end 
+  return u end
+
+function lib.copy(t)         
+  return type(t) ~= 'table' and t or lib.map(t, lib.copy) end
 
 function lib.keys(t,   ks)
-  ks={}; for k,_ in pairs(t) do ks[1+#ks]=k end; return sort(ks) end
+  ks={}; for k,_ in pairs(t) do ks[1+#ks]=k end; return lib.sort(ks) end
 
 function lib.isa(mt,t) return setmetatable(t, mt) end
+
+function lib.first(x,y) return x[1] < y[1] end
 
 -- ### Rand
 function lib.randi(lo,hi) return math.floor(0.5 + lib.rand(lo,hi)) end
 function lib.rand(lo,hi,     mult,mod)
   lo, hi = lo or 0, hi or 1
-  Seed = (16807 * Seed) % 2147483647 
-  return lo + (hi-lo) * Seed / 2147483647 end 
+  lib.Seed = (16807 * lib.Seed) % 2147483647 
+  return lo + (hi-lo) * lib.Seed / 2147483647 end 
+
+-- ### Files
+function lib.csv(file,      split,stream,tmp)
+  stream = file and io.input(file) or io.input()
+  tmp    = io.read()
+  return function(       t)
+    if tmp then
+      t,tmp = {},tmp:gsub("[\t\r ]*",""):gsub("#.*","")
+      for y in string.gmatch(tmp, "([^,]+)") do t[#t+1]=y end
+      tmp = io.read()
+      if  #t > 0
+      then for j,x in pairs(t) do t[j] = tonumber(x) or x end
+           return t end
+    else io.close(stream) end end end
 
 
 -- ### Printing
@@ -51,57 +75,56 @@ function lib.out(t,         u,secret,brace,out1,show)
   elseif #t>0                then return brace(lib.map(t, lib.out1),", ") 
   else   u={}
          for k,v in pairs(t) do if not secret(k) then u[1+#u] = {k,v} end end
-         return lib.yellow(t._is or "")..brace(lib.map(lib.sort(u,first), show)) end end
-
-
+         return lib.yellow(t._is or "")..brace(
+                                           lib.map(
+                                             lib.sort(u,lib.first), 
+                                             show)) end end
 
 -- -------------------------------------------------------------
 -- ### Start-up
-function lib.cli(   u)
+function lib.cli(listOfFours,   u)
   u={}
-  for _,t in pairs(about()) do
+  for _,t in pairs(listOfFours) do
     u[t[1]] = t[3]
     for n,word in ipairs(arg) do if word==t[2] then
       u[t[1]] = (t[3]==false) and true or tonumber(arg[n+1]) or arg[n+1] end end end 
   return u end 
 
-function lib.help(s)
-  print(s.." [OPTIONS]\n\nOPTIONS:\n"); 
-  for _,t in pairs(about()) do
+function lib.help(usage,listOfFours)
+  print(usage.." [OPTIONS]\n\nOPTIONS:\n"); 
+  for _,t in pairs(listOfFours) do
     print(lib.fmt("  %-4s%s %s",t[2],lib.fmt("%-10s",t[3]==false and "" or t[3]),t[4])) end 
   print("\nSTART-UP ACTIONS:\n"); lib.go("ls") end
 
 -- -------------------------------------------------------------
 -- ### Unit tests
-function lib.main(b4, usage, about)
-  if   about().help 
-  then help(usage)
-  else lib.go(about().todo) 
-  end
+lib.Eg={}
+lib.fails= -1
+
+function lib.main(b4, usage, listOfFours,  the)
+  the = lib.cli(listOfFours)
+  if the.help then lib.help(usage, listOfFours) else lib.go(the.todo, the) end
   for k,v in pairs(_ENV) do if not b4[k] then 
     print("?? ",k,type(v)) end end 
-  os.exit(fails) end
+  os.exit(lib.fails) end
 
-function lib.go(x,about,     ok,msg) 
-  the = about()
-  Seed = the.seed 
-  if the.wild then return eg[x][2]() end
-  ok, msg = pcall(eg[x][2])
+function lib.go(x,the,     ok,msg) 
+  lib.Seed = the.seed 
+  if the.wild then return lib.Eg[x][2](the) end
+  ok, msg = pcall(lib.Eg[x][2], the)
   if   ok 
   then print(lib.green("PASS: "),x) 
   else print(lib.red("FAIL: "),x,msg)
-       eg._fails = eg._fails + 1 end end
+       lib.fails = lib.fails + 1 end end
 
 -- ## Examples
-eg. _fails= -1
-eg.ls={"list all examples", function () 
-  lib.map(lib.keys(eg), function (_,k) 
-    print(fmt("  -t  %-10s ",k)..eg[k][1]) end) end}
+lib.Eg.ls={"list all examples", function (_) 
+  lib.map(lib.keys(lib.Eg), function (_,k) 
+    print(lib.fmt("  -t  %-10s ",k)..lib.Eg[k][1]) end ) end}
 
-eg.all={"run all examples", function() 
-  lib.map(lib.keys(eg),function(_,k) 
-                 return k ~= "all" and k ~= "ls" and lib.go(k) end) end}
+lib.Eg.all={"run all examples", function(the) 
+  lib.map(lib.keys(lib.Eg),
+    function(_,k)  
+      return k ~= "all" and k ~= "ls" and lib.go(k, lib.copy(the)) end) end}
 
-eg.fail={"demo failure", function () assert(false,"oops") end}
-
-return eg,lib
+return lib
