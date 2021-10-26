b5044dc85097cfaf959881e6bb7b389a95853942
Tim Menzies <timm@ieee.org>
2021-10-25 20:16:45 -0400

    save

--- src/lib.lua
+++ src/lib.lua
@@ -1,130 +1,64 @@
-local lib={}
+local function obj(is,  o) 
+  o={_is=is,__tostring=out}; o.__index=o; return o end
 
--- ## Classes
--- Columns of data either `Num`eric, `Sym`bolic, or things we are going to `Skip` over.
--- `Sample`s hold rows of data, summarized into `Cols` (columns).
-function lib.obj(is,   k) 
-  k={_is=is,__tostring=function(x) return out(x) end}; k.__index=k; return k end
+local function atom(s,b4) 
+  return (b4==false and true) or tonumber(s) or s end
 
--- -------------------------------------------------------------
--- ## Lib
--- ### The Usual Short-cuts
-lib.cat  = table.concat
-lib.fmt  = string.format
-lib.push = table.insert
-lib.sort = function(t,f) table.sort(t,f); return t end
+local function flag(it,b4) 
+  for n,s in ipairs(arg) do 
+    if s==it then return atom(arg[n+1],b4) end end end
 
-function lib.map(t,f,      u) 
-  f = f or function(k,v) return v end
-  u={}
-  for k,v in pairs(t) do 
-    local tmp = f(k,v)
-    if tmp then u[k] = tmp end end 
-  return u end
+--  Short-cuts
+local ee   = math.exp(1)
+local abs  = math.abs
+local cat  = table.concat
+local fmt  = string.format
+local push = table.insert
+local sort = function(t,f) table.sort(t,f); return t end
+local isa  = function(mt,t) return setmetatable(t, mt) end
 
-function lib.copy(t)         
-  return type(t) ~= 'table' and t or lib.map(t, lib.copy) end
+--  Lists
+local function map(t,f,  u) 
+  u={}; for k,v in pairs(t) do u[k]=f(k,v) end; return u end 
 
-function lib.keys(t,   ks)
-  ks={}; for k,_ in pairs(t) do ks[1+#ks]=k end; return lib.sort(ks) end
+local function keys(t,   k) 
+  k={}; for x,_ in pairs(t) do 
+    if tostring(x):sub(1,1)~="_" then push(k,x) end end 
+  return sort(k) end
 
-function lib.isa(mt,t) return setmetatable(t, mt) end
+local function kopy(obj,seen,    s,out)
+  if type(obj) ~= 'table' then return obj end
+  if seen and seen[obj]   then return seen[obj] end
+  s,out = seen or {},{}
+  s[obj] = out
+  for k, v in pairs(obj) do out[kopy(k, s)] = kopy(v, s) end
+  return setmetatable(out, getmetatable(obj)) end
 
-function lib.first(x,y) return x[1] < y[1] end
+--  Printing
+local function shout(t) print(out(t)) end
 
--- ### Rand
-function lib.randi(lo,hi) return math.floor(0.5 + lib.rand(lo,hi)) end
-function lib.rand(lo,hi,     mult,mod)
-  lo, hi = lo or 0, hi or 1
-  lib.Seed = (16807 * lib.Seed) % 2147483647 
-  return lo + (hi-lo) * lib.Seed / 2147483647 end 
+local function out(t,    u,f1,f2)
+  function f1(_,x) return fmt(":%s %s",x,out(t[x])) end
+  function f2(_,x) return out(x) end
+  if type(t) ~= "table" then return tostring(t) end
+  u=#t==0 and map(keys(t),f1) or map(t,f2)
+  return (t._is or"").."{"..cat(u,", ").."}" end
 
--- ### Files
-function lib.csv(file,      split,stream,tmp)
+--  Files
+local function csv(file,      split,stream,tmp)
   stream = file and io.input(file) or io.input()
   tmp    = io.read()
   return function(       t)
-    if tmp then
-      t,tmp = {},tmp:gsub("[\t\r ]*",""):gsub("#.*","")
-      for y in string.gmatch(tmp, "([^,]+)") do t[#t+1]=y end
-      tmp = io.read()
-      if  #t > 0
-      then for j,x in pairs(t) do t[j] = tonumber(x) or x end
-           return t end
+    if   tmp 
+    then t,tmp = {},tmp:gsub("[\t\r ]*",""):gsub("#.*","")
+         for y in string.gmatch(tmp, "([^,]+)") do push(t,y) end
+         tmp = io.read()
+         if  #t > 0
+         then for j,x in pairs(t) do t[j] = atom(x) end
+              return t end
     else io.close(stream) end end end
 
-
--- ### Printing
--- colored strings
-function lib.red(s)    return "\27[1m\27[31m"..s.."\27[0m" end
-function lib.green(s)  return "\27[1m\27[32m"..s.."\27[0m" end
-function lib.yellow(s) return "\27[1m\27[33m"..s.."\27[0m" end
-function lib.blue(s)   return "\27[1m\27[36m"..s.."\27[0m" end
-
--- Print a generated string
-function lib.shout(x) print(lib.out(x)) end
--- Generate a string, showing sorted keys, hiding secretes (keys starting with "_")
-
-function lib.out(t,         u,secret,brace,out1,show)
-  function secret(s) return tostring(s):sub(1,1)== "_" end
-  function brace(t)  return "{"..lib.cat(t,", ").."}" end
-  function out1(_,v) return lib.out(v) end
-  function show(_,v) return lib.fmt(":%s %s", lib.blue(v[1]), lib.out(v[2])) end
-  if     type(t)=="function" then return "#`()"
-  elseif type(t)~="table"    then return tostring(t) 
-  elseif #t>0                then return brace(lib.map(t, lib.out1),", ") 
-  else   u={}
-         for k,v in pairs(t) do if not secret(k) then u[1+#u] = {k,v} end end
-         return lib.yellow(t._is or "")..brace(
-                                           lib.map(
-                                             lib.sort(u,lib.first), 
-                                             show)) end end
-
--- -------------------------------------------------------------
--- ### Start-up
-function lib.cli(listOfFours,   u)
-  u={}
-  for _,t in pairs(listOfFours) do
-    u[t[1]] = t[3]
-    for n,word in ipairs(arg) do if word==t[2] then
-      u[t[1]] = (t[3]==false) and true or tonumber(arg[n+1]) or arg[n+1] end end end 
-  return u end 
-
-function lib.help(usage,listOfFours)
-  print(usage.." [OPTIONS]\n\nOPTIONS:\n"); 
-  for _,t in pairs(listOfFours) do
-    print(lib.fmt("  %-4s%s %s",t[2],lib.fmt("%-10s",t[3]==false and "" or t[3]),t[4])) end 
-  print("\nSTART-UP ACTIONS:\n"); lib.go("ls") end
-
--- -------------------------------------------------------------
--- ### Unit tests
-lib.Eg={}
-lib.fails= -1
-
-function lib.main(b4, usage, listOfFours,  the)
-  the = lib.cli(listOfFours)
-  if the.help then lib.help(usage, listOfFours) else lib.go(the.todo, the) end
-  for k,v in pairs(_ENV) do if not b4[k] then 
-    print("?? ",k,type(v)) end end 
-  os.exit(lib.fails) end
-
-function lib.go(x,the,     ok,msg) 
-  lib.Seed = the.seed 
-  if the.wild then return lib.Eg[x][2](the) end
-  ok, msg = pcall(lib.Eg[x][2], the)
-  if   ok 
-  then print(lib.green("PASS: "),x) 
-  else print(lib.red("FAIL: "),x,msg)
-       lib.fails = lib.fails + 1 end end
-
--- ## Examples
-lib.Eg.ls={"list all examples", function (_) 
-  lib.map(lib.keys(lib.Eg), function (_,k) 
-    print(lib.fmt("  -t  %-10s ",k)..lib.Eg[k][1]) end ) end}
-
-lib.Eg.all={"run all examples", function(the) 
-  lib.map(lib.keys(lib.Eg),
-    function(_,k)  
-      return k ~= "all" and k ~= "ls" and lib.go(k, lib.copy(the)) end) end}
-
-return lib
+return {
+  obj=obj,atom=atom, flag=flag,
+  ee=ee,abs=abs,cat=cat,fmt=fmt,push=push,isa=isa,sort=sort,
+  keys=keys,map=map,kopy=kopy,out=out,shout=shout,csv=csv}
--- src/notbad.lua
+++ src/notbad.lua
@@ -1,9 +1,9 @@
 local b4={}; for k,v in pairs(_ENV) do b4[k]=v end
-local the,Skip, Num,Sym,Sample, Syms,Nyms
-local ee, abs,cat,fmt,push,same,sort, keys,map,out,shout,csv 
-local function obj(is,  o) o={_is=is,__tostring=out}; o.__index=o; return o end
-local function atom(b4,s) return (b4==false and true) or tonumber(s) or s end
-local function flag(it,b4) for n,s in ipairs(arg) do if s==it then return atom(b4,arg[n+1]) end end end
+local the,Skip, Num,Sym,Sample, Syms,Nums
+local _ = require"lib"
+local obj,atom,flag                = _.obj, _.atom, _.flag
+local ee,abs,cat,fmt,push,isa,sort = _.ee,_.abs,_.cat,_.fmt,_.push,_.isa,_.sort
+local keys,map,out,shout,csv       = _.keys,_.map,_.out,_.shout,_.csv
 
 --  Settings, CLI
 the = {
@@ -93,76 +93,39 @@ function Syms.new(t,    self)
   self= isa(Syms,{has={},n=0,mode="?",most=0}) 
   for x,n in pairs(t or {}) do self:add(x,n) end
   return self end 
+
 function Syms:add(x,inc)  
   inc = inc or 1
   self.n = self.n + inc 
   self.has[x] = inc + (self.has[x] or 0)
   if self.has[x] > self.most then self.most,self.mode=self.has[x],x end  end
-function Syms:any()
-  local r=math.random(self.n)
+
+function Syms:any(     r)
+  r = math.random(self.n)
   for x,n1 in pairs(self.has) do r=r-n1; if r <=0 then return x end end  
   return self.most end
 
 Nums=obj"Nums"
 function Nums.new(lo,hi,bins) 
   return isa(Nums,{lo=lo or 0, hi=hi or 1, bins=bins or 16, syms=Syms.new()}) end
+
 function Nums:add(x,inc,    y)
-  assert(x>=self.lo,"too small")
-  assert(x<=self.hi,"too big")
+  assert(x>=self.lo, "too small")
+  assert(x<=self.hi, "too big")
   y = ((x - self.lo)/(self.hi -  self.lo) * self.bins // 1)
   self.syms:add(y,inc)  end
+
 function Nums:any(      bin)
   bin=self.syms:any()
   return self.lo + (bin + math.random())*(self.hi - self.lo)/self.bins end
 
------------------------------------------------
---  Short-cuts
-ee   = math.exp(1)
-abs  = math.abs
-cat  = table.concat
-fmt  = string.format
-push = table.insert
-sort = function(t,f) table.sort(t,f); return t end
-isa  = function(mt,t) return setmetatable(t, mt) end
-
---  Lists
-function map(t,f,  u) 
-  u={}; for k,v in pairs(t) do u[k]=f(k,v) end; return u end 
-function keys(t,   k) 
-  k={}; for x,_ in pairs(t) do 
-    if tostring(x):sub(1,1)~="_" then push(k,x) end end ; return sort(k) end
-
--- ## Printing
-function shout(t) print(out(t)) end
-function out(t,    u,f1,f2)
-  if type(t) ~= "table" then return tostring(t) end
-  function f1(_,x) return fmt(":%s %s",x,out(t[x])) end
-  function f2(_,x) return out(x) end
-  u=#t==0 and map(keys(t),f1) or map(t,f2)
-  return (t._is or"").."{"..cat(u,", ").."}" end
-
-
---  Files
-function csv(file,      split,stream,tmp)
-  stream = file and io.input(file) or io.input()
-  tmp    = io.read()
-  return function(       t)
-    if   tmp 
-    then t,tmp = {},tmp:gsub("[\t\r ]*",""):gsub("#.*","")
-         for y in string.gmatch(tmp, "([^,]+)") do push(t,y) end
-         tmp = io.read()
-         if  #t > 0
-         then for j,x in pairs(t) do t[j] = tonumber(x) or x end
-              return t end
-    else io.close(stream) end end  end
-
 local  n=Nums.new(0,10,10)
-tmp={}
+local  tmp={}
 for i=1,100000 do push(tmp, 10*(math.random()^.5)//1) end
 --for _,x in pairs(tmp) do print(x) end
-for _,x in pairs(tmp) do n:add(x) end
-for i=1,100000 do x=n:any()//1 end
-
+-- for _,x in pairs(tmp) do n:add(x) end
+-- for i=1,100000 do x=n:any()//1 end
+--
 -- b=Syms.new{bananas=10,apples=20,oranges=40}
 -- for _ = 1,1000 do print(b:any()) end
 --
