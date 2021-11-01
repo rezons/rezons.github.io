71d3c633a94f2303b496ef3f33b674697844e689
Tim Menzies <timm@ieee.org>
2021-11-01 11:03:27 -0400

    save

--- src/ssl.lua
+++ src/ssl.lua
@@ -1,9 +1,8 @@
 local b4={}; for k,v in pairs(_ENV) do b4[k]=v end
-local Seed,rand
-
-the = {some=8}
+local the = {stop=.5, some=8}
 
 -- shorties
+local cat,fmt,push,sort,noop
 cat  = table.concat
 fmt  = string.format
 push = table.insert
@@ -11,6 +10,7 @@ sort = function(t,f) table.sort(t,f); return t end
 noop = function(x,...) return x  end
 
 -- Random
+local Seed, randi, rand
 Seed=937162211
 function randi(lo,hi) return math.floor(0.5 + rand(lo,hi)) end
 function rand(lo,hi,     mult,mod)
@@ -18,6 +18,8 @@ function rand(lo,hi,     mult,mod)
   Seed = (16807 * Seed) % 2147483647 
   return lo + (hi-lo) * Seed / 2147483647 end 
 
+-- lists
+local map,keys,shuffle
 function map(t,f,  u) 
   u={}; for k,v in pairs(t) do u[k]=f(k,v) end; return u end 
 
@@ -26,23 +28,13 @@ function keys(t,   k)
     if tostring(x):sub(1,1)~="_" then push(k,x) end end 
   return sort(k) end
 
-function kopy(obj,seen,    s,out)
-  if type(obj) ~= 'table' then return obj end
-  if seen and seen[obj]   then return seen[obj] end
-  s,out = seen or {},{}
-  s[obj] = out
-  for k, v in pairs(obj) do out[kopy(k, s)] = kopy(v, s) end
-  return setmetatable(out, getmetatable(obj)) end
-
 function shuffle(t,n,    j)
   for i = #t,2,-1 do j=randi(1,i); t[i],t[j] = t[j],t[i] end
   return t end
 
-function top(n,t,      u)
-  u={}; for m,x in pairs(t) do u[m]=x; if m>=n then break end end
-  return u end
-
+function  firsts(x,y) return x[1] < y[1] end
 --  Printing
+local shout,out
 function shout(t) print(out(t)) end
 
 function out(t,    u,f1,f2,blue,yellow)
@@ -53,6 +45,7 @@ function out(t,    u,f1,f2,blue,yellow)
   return (t._is or"").."{"..cat(u,", ").."}" end
 
 -- CSV reading
+local csv
 function csv(file,      split,stream,tmp)
   stream = file and io.input(file) or io.input()
   tmp    = io.read()
@@ -67,17 +60,37 @@ function csv(file,      split,stream,tmp)
     else io.close(stream) end end end
 
 -- Meta
+local isa,obj
 function isa(mt,x) return setmetatable(x,mt) end
 function obj(s, o) o={_is=s, __tostring=out}; o.__index=o; return o end
 
+-- Cols
+local Sym,Num,Skip,Cols,Sample
+local klassp,goalp,nump,ako
+function klassp(v) return v:find"!" end
+function nump(v)   return v:match("^[A-Z]") end
+function goalp(v)  return klassp(v)  or v:find"+" or v:find"-" end
+function ako(v)    return (v:find":" and Skip) or (nump(v) and Num) or Sym end
+
+Cols= obj"Cols"
+function Cols.new(lst,       self,now) 
+  self = isa(Cols, {all={},xs={},ys={},klass=nil}) 
+  for k,v in pairs(lst) do
+    now = ako(v).new(k,v)
+    push(self.all, now)
+    if not v:find"?" then 
+      if klassp(v) then self.klass=now end
+      push(klassp(v) and self.ys or self.xs, now) end end
+  return self end
+
 -- Sym
-local Sym = obj"Sym"
+Sym = obj"Sym"
 function Sym.new(i,s)  return isa(Sym, {at=i,txt=s,has={}}) end
 function Sym:dist(x,y) return  x==y and 0 or 1 end
 function Sym:add(x)    if x=="?" then return x end; self.has[x]=1+(self.has[x] or 0) end
 
 -- Num
-local Num = obj"Num"
+Num = obj"Num"
 function Num.new(i,s) 
   return isa(Num, {at=i,txt=s,hi=-1E32,lo=1E32,w=s:find"-" and -1 or 1}) end
 
@@ -86,8 +99,8 @@ function Num:add(x)
   self.lo = math.min(x, self.lo) 
   self.hi = math.max(x, self.hi)  end
 
-function Num:norm(x)
-  local lo,hi = self.lo,self.hi
+function Num:norm(x,     lo,hi)
+  lo,hi = self.lo,self.hi
   return math.abs(hi-lo)< 1E-16 and 0 or (x-lo)/(hi-lo) end
 
 function Num:dist(x,y)
@@ -97,32 +110,16 @@ function Num:dist(x,y)
   return math.abs(x-y) end
 
 -- Skip
-local Skip= obj"Skip"
+Skip= obj"Skip"
 function Skip.new(i,s) return isa(Skip,{at=i,txt=s}) end
 function Skip:add(x) return x end
 
 -- Cols
-local Cols= obj"Cols"
-
-function klassp(v) return v:find"!" end
-function nump(v)   return v:match("^[A-Z]") end
-function ako(v)    return (v:find":" and Skip) or (nump(v) and Num) or Sym end
-
-function Cols.new(lst,       self,now) 
-  self = isa(Cols, {all={},xs={},ys={},klass=nil}) 
-  for k,v in pairs(lst) do
-    now = ako(v).new(k,v)
-    push(self.all, now)
-    if not v:find"?" then 
-      if klassp(v) then self.klass=now end
-      push(nump(v) and self.ys or self.xs, now) end end
-  return self end
-
 -- Sample
 local Sample= obj"Sample"
 function Sample.new() return isa(Sample, {rows={}, cols=nil}) end
 
-function  Sample:add(lst,   ako)
+function  Sample:add(lst,   add)
   function add(k,v) self.cols.all[k]:add(v); return v; end  
   if   not self.cols 
   then self.cols = Cols.new(lst) 
@@ -131,6 +128,8 @@ function  Sample:add(lst,   ako)
 function Sample:dist(row1,row2)
   local d,n,p,x,y,inc
   d, n, p = 0, 1E-32, 2
+  print(2000)
+  shout(self.cols.xs)
   for _,col in pairs(self.cols.xs) do
     x,y = row1[col.at], row2[col.at]
     inc = x=="?" and y=="?" and 1 or col:dist(x,y)
@@ -145,31 +144,37 @@ function Sample:better(row1,row2)
   for _,col in pairs(cols) do
     a  = col:norm(row1[col.at])
     b  = col:norm(row2[col.at])
-    w  = col.w -- w = (1,-1) if (maximizing,m inimizing)
+    w  = col.w 
     s1 = s1 - e^(col.w * (a - b) / n)
     s2 = s2 - e^(col.w * (b - a) / n) end
   return s1 / n < s2 / n end
 
-function Sample:div(rows,top)
+function Sample:div(rows,some,stop)
+  local function betters(x,y) return self:better(x,y) end
   rows = rows or shuffle(self.rows)
-  top  = top or #rows  
-  some = {}
-  for i= top,top-some,-1 do push(some, rows[i]) end
-  table.sort(some, function(x,y) return self:better(x,y) end)
-  for i= top-some-1,1,-1 do
-    row1, d, j = rows[i], 1E32, some // 2
-    for k,row2 in pairs(some) do
-      tmp = self:dist(row1,row2) 
-      if tmp<d then d,j = tmp,k end end end 
-end
+  some = some or the.some
+  stop = stop or (#rows)^the.stop
+  if #rows <= 2*stop or #rows <= 2*some then return rows end
+  local somes, best  = {},{}
+  for i,row in pairs(rows) do
+    if i <= some
+    then push(somes, row) 
+    else if i==some then table.sort(somes, betters) end
+         local closest, rowRank = 1E32, 1E32
+         for someRank,some1 in  pairs(somes) do
+           local dist = self.dist(row,some1)
+           if dist < closest then closest, rowRank = dist, someRank end end
+         if rowRank <= some//2 then push(best, row) end end end
+  return self:div(best,some,stop) end
 
 -- Main
-function main(file,     s)
+local function main(file,     s)
   s = Sample.new()
   for row in csv(file) do s:add(row) end
-  shout(s.cols.all)
+  shout(s.cols.xs)
+  s:div()
 end
 
 main("../data/auto93.csv")
-for k,v in pairs(_ENV) do if not b4[k] and type(v)~="function" then print("? ",k,type(v)) end end
+for k,v in pairs(_ENV) do if not b4[k] then print("? ",k,type(v)) end end
 --for row in csv("../data/auto93.csv") do shout(row) end
