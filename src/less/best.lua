7fd7f296ddc5d7bf84dd77dcab53155e95be6ae0
Tim Menzies <timm@ieee.org>
2021-11-14 13:37:03 -0500

    save

--- src/less/best.lua
+++ src/less/best.lua
@@ -1,27 +1,29 @@
 local my      = require"my"
 local obj,has = my.get"metas obj has"
-local rand    = my.get"rands  rand"
+local rand    = my.get"rands rand"
+
+local key=function(x) return x[1] end
+local val=function(x) return x[2] end
+local firsts=function(x,y) return  x[1] < y[1] end
 
 local Best=obj"Best"
-function Best.new(inits,  self) 
-  return has(Best,{keep=my.keep,
-                   total= sum(inits, function(x) return x[1] end),
-                   all =sort(inits, function(x,y) return  x[1] < y[1] end)}) end
+function Best.new(keep,inits,  self) return has(Best,
+  {keep=keep or 10, total=sum(inits, key), all=sort(inits, firsts)}) end
 
 function Best:add(k,v, border,pos)
   border = #self.all - self.keep
   if border<1 then border = #self.all//2 end
-  if #self.all < self.keep or k > self.all[border][1] then
+  if #self.all < self.keep or k > key(self.all[border])then
     pos = bchop(self.all,{k,v},firsts)
     push(self.best,pos,{k,v}) 
     self.total = self.total + k 
   end end 
 
 function Best:one()
-  local r=rand()*self.all
+  local r=rand()*self.total
   for i = #self.all,1,-1 do
-    r = r - self.all[i][1]
-    if r<=0 then return self.all[i][2] end end
-  return self.all[1][2] end
+    r = r - key(self.all[i])
+    if r<=0 then return val(self.all[i]) end end
+  return val(self.all[1]) end
 
 return Best
