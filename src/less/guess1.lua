0eb34d0106f83a23f290f2a101854373198abfd2
Tim Menzies <timm@ieee.org>
2021-11-13 23:00:07 -0500

    save

--- src/less/guess1.lua
+++ src/less/guess1.lua
@@ -16,17 +16,18 @@ task ={n= 60,
        y= {y1=function(x,w) return poly3(x.x1,w.x0, w.a, w.b, w.c) end}}
 
 function evals(task,out)
-  local x,w,y,d,p,any,mre,eval,want,got
+  local x1,w,y,d,p,any,mre,eval,want,got
   d = d or 2
   function p(x)      return round(x,d) end
   function any(_,x)  return p(rand(x[1],x[2])) end
   function eval(_,f) return p(f(x,w)) end
   for i=1,task.n do 
-    x = sort(map(task.x,any))
-    w = sort(map(task.w,any))
-    y = sort(map(task.y, eval))
-    want=task.truth.y1(x)
-    push(out, {abs((y.y1 - want)/want), w}) end 
+    local now = {}
+    now.x = map(task.x, any)
+    now.c = map(task.c, any)
+    now.y = map(task.y, function(_,f) return f(now.x,now.w) end) 
+    want  = task.truth.y1(now.x)
+    push(out, {abs((now.y.y1 - want)/want), w}) end 
   return out end
 
 my = cli(my.how, arg)
