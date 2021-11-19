841d1c3007f38a369529c8b3bc6678f9ff74b457
Tim Menzies <timm@users.noreply.github.com>
2021-11-19 15:17:17 -0500

    save

--- src/less/magic.lua
+++ src/less/magic.lua
@@ -1,4 +1,4 @@
-local function generateHelpString(opt,    show,b4,s)
+local function helpString(opt,    show,b4,s)
   s= opt.what .. "\n" .. opt.when .. "\n\nOPTIONS:"
   function show(x) 
     if #x[1]>0 and x[1] ~= b4 then s=s.."\n"..x[1]..":\n" end
@@ -15,17 +15,23 @@ local function updateFromCommandLine(fours,    x)
     x[t[2]] = (t[4]==false) and true or tonumber(arg[n+1]) or arg[n+1] end end end 
   return x end
 
-local function getSomeFunctionsFromFile(_,s,   t,u)
+local function someFunsFromFile(s,   t,u)
   for x in s:gmatch("%w+") do 
     if not t then t,u = require(x),{} else u[ 1+#u ] = t[x] end end
   return table.unpack(u) end
+  
+local b4={}; for k,_ in pairs(_ENV) do b4[k]=k end
 
-return function (t,  b4,the)
-  b4={}; for k,_ in pairs(_ENV) do b4[k]=k end
-  the = updateFromCommandLine(t.how)
-  the._etc={}
-  the._etc= {help   = generateHelpString(t),
-             rogues = function ()
-                        for k,v in pairs(_ENV) do if not b4[k] then 
-                          print("?? rogue",k,type(v)) end end end}
-  return setmetatable(the,{__call=getSomeFunctionsFromFile}) end
+local function rogues(b4)
+  for k,v in pairs(_ENV) do 
+    if not b4[k] then 
+      print("?? rogue",k,type(v)) end end end
+
+return function(t)
+  local function what2do(_,thing)
+    if thing=="END"  then return rogues(b4)    end
+    if thing=="HELP" then return helpString(t) end
+    return someFunsFromFile(thing)  
+  end --
+  return setmetatable(updateFromCommandLine(t.how), {__call=what2do}) end
+
