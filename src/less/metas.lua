83e5de6d452f2f7d780863ecae0c26723f7755d0
Tim Menzies <timm@ieee.org>
2021-11-19 02:25:03 -0500

    save

--- src/less/metas.lua
+++ src/less/metas.lua
@@ -1,9 +1,15 @@
+local out=require("prints").out
 local has,obj
 
 -- Instance  creation
 function has(mt,x) return setmetatable(x,mt) end
 
 -- Object creation.
-function obj(s, o) o={_is=s, __tostring=out}; o.__index=o; return o end
+function obj(s, o) 
+   o={ _is        = s,
+       __tostring = out,
+       __call     = function(_,...) return o.new(...) end}
+   o.__index=o
+   return setmetatable(o,o) end 
 
 return {has=has, obj=obj}
