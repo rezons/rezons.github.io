7a1ca6ea701c268a9a8c99059909293ce7695cbd
Tim Menzies <timm@ieee.org>
2021-11-21 23:46:47 -0500

    save

--- src/less/guess3.lua
+++ src/less/guess3.lua
@@ -28,10 +28,10 @@ function Num:add(x)
   return x end
 function Num:adds(t) 
   for _,x in pairs(t) do self:add(x) end; return self end
-function Num:any()   
+function Num:any(    z)   
   if   self.new 
-  then return self.lo+r()*(self.hi - self.lo) 
-  else return self.mu+self.sd*sqrt(-2*log(r()))*cos(2*pi*r()) end end 
+  then  return self.lo+r()*(self.hi - self.lo)
+  else  return self.mu+self.sd*sqrt(-2*log(r()))*cos(2*pi*r()) end end 
 function Num:mid() 
   return self.mu end
 function Num:norm(x)
@@ -91,55 +91,64 @@ function Cols:clone()
   return Cols(self.header) end
 function Cols:mid(cols)
   return map(cols or self.all,function(_,col) return col:mid() end) end
-function Cols:zeroOne()
+function Cols:range(lo,hi)
   for _,col in pairs(self.all) do 
-    if col._is=="Num" then col.lo=0; col.hi=1 end end
+    if col._is=="Num" then col.lo=lo or 0; col.hi=hi or 1 end end
   return self end
   
 srand(the.seed)
 -------------------------------------------------------------------------------
-local function zdt1(t)
+local function zdt1(t,  d)
   local f1,g,h,f2
   for i=1,(d or 10) do t[i]=r() end
   f1 = t[1]
-  g  = 0; for i=2,#t do g = g + t[i] / (#t - 1) end
-  g  = 1 + 9 * g
+  g  = 0; for i=2,#t do g = g + t[i] end
+  g  = 1 + 9 / (#t-1) * g
   h  = 1 - (f1 / g)^.5
   t[1+#t] = f1
   t[1+#t] = g*h
   return t end
 
-local rnd2,rnd3,insert,nCrossEntropy
+local rnd2,rnd3,with,nCrossEntropy
 
-function rnd2(x) return round(x,2) end
-function rnd3(x) return round(x,3) end
-function insert(t,   u) for k,v in pairs(t) do u[k]=v end; return u end
+function r2(x) return round(x,2) end
+function r3(x) return round(x,3) end
+function r3s(t) return map(t,function(_,x) return round(x,3) end) end
+function with(t,   u) 
+  t, u = t or {}, u or {}
+  for k,v in pairs(t) do u[k]=v end; return u end
 
-function nCrossEntropy(it0,        best,it,xy,ok,now,b4,ys,all)
-  it = insert(it0, {verbose=false,m=10,n=100,top=.1})
+function nCrossEntropy(it0,        tmp,first,best,it,xy,ok,now,b4,ys)
+  it = with(it0, {verbose=false,m=10,n=100,top=.1})
   b4 = it.before
   function xy()    return it.f( b4:any(b4.xs) ) end
   function ok(a,b) return b4:better(a,b) end 
-  all=b4:clone()
   for i = 1,it.generations do
     now = b4:clone()
-    for _,one in pairs(top(it.n*it.top, sort(ntimes(it.n, xy), ok))) do
-      all:add(one)
+    tmp=sort(ntimes(it.n, xy), ok)
+    if i==1 then
+      shout(r3s(tmp[1]))
+      shout(r3s(tmp[#tmp//2]))
+      shout(r3s(tmp[#tmp])) end
+    for _,one in pairs(top(it.n*it.top, tmp)) do
       now:add(one) end
-    if it.verbose then print(out(now:mid(now.ys)), 
-                             out(all:mid(all.ys))) end
+    if i==1 then first=now end
+    if it.verbose then print( out(now:mid(now.ys))) end
     b4 = now 
   end
-  return now end
+  return first,now end
 
 srand(the.seed)
-local xs,ys = nCrossEntropy {
+local first,last = nCrossEntropy {
    generations = 10,
-   n       = 100,
-   top     = .1;
-   verbose = true,
-   before  = Cols({"X1","X2","X3","X4","X5","Y1-","Y2-"}):zeroOne(),
+   n       = 1000,
+   top     = .2;
+   verbose = false,
+   before  = Cols({"X1","X2","X3","X4","X5","Y1-","Y2-"}):range(),
    f       = zdt1}
+
+shout(first:mid(first.ys))
+shout(last:mid(last.ys))
 
 -- lean {
 --   max=1000, wait=10,  pause=100, 
