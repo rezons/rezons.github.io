7256e01b6367a4de3145842d898a20834ccef070
Tim Menzies <timm@ieee.org>
2021-11-28 11:21:41 -0500

    save

--- src/sandbox/b.lua
+++ src/sandbox/b.lua
@@ -1,43 +1,67 @@
 local b4={}; for k,_ in pairs(_ENV) do b4[k]=k end
 ----------------------------------------------
-
+local function map(t,f,  u,y,z) 
+  u={}
+  for k,x in pairs(t) do 
+    y,z = f(k,x) 
+    if y ~= nil then
+      if z==nil then u[#u+1]=y else u[y]=x end end end
+  return u end
+----------------------------------------------
 -- Call `f(key,value)` on all items  in list.
-local lunatic, Lambda, Hint, From, Local, Return, Self
-local fmt = string.format
+local lunatic, inquire
 
-function lunatic(s,todo,      act)
+function lunatic(s,control)
+  local Local,Return,Self,Lambda,Hint,From,act,fmt
+  fmt = string.format
+  function Local(s) return s:gsub("`","local ") end
+
+  function Self(s) return s:gsub("%f[%w_]o%f[^%w_]","self") end
+
+  function Return(s) 
+    return s:gsub("(%s)^",
+      function(b4) return b4.." return " end) end
+
+  function Lambda(s) 
+    return s:gsub("(`)(%b())", 
+      function(_,b) return fmt("function %s end", b:sub(2,#b-1)) end) end
+
+  function Hint(s)
+    return s:gsub("(function[^\n]+)(%b())(%s*:[^\n]*)\n",
+      function(a,b,c) return fmt("%s%s\n",a,b:gsub(":[^,)]+","")) end) end
+
+  function From(s,        act)
+    function act (file,wants,     sep,pre,post) 
+      sep,pre,post = "","",""
+      for want in wants:gmatch("([^ ]+)") do 
+        pre  = pre  .. sep .. want
+        post = post .. sep .. fmt('require("%s").%s',file,want) 
+        sep  = ", " end
+      return fmt("\nlocal %s = %s\n",pre,post) 
+    end
+    return s:gsub("\nfrom%s+([^\n]*)%s+import([^\n]*)\n", act) end
+
   function act(t)
-    for k,f in pairs(t) do if (todo or {})[k] ~= false then s=f(s) end end 
-  end
+    for k,f in pairs(t) do 
+      if (control or {})[k] ~= false then s=f(s) end end end
+
   act{Lambda=Lambda}
   act{Local=Local,Self=Self,Return=Return,From=From,Hint=Hint} 
   return s end
-function Local(s)  return s:gsub("`","local ") end
-function Return(s) return s:gsub("(%s)^"," return ")end
-function Self(s)   return s:gsub("%f[%w_]o%f[^%w_]","self") end
-function Lambda(s) 
-  return s:gsub("(`)(%b())", 
-    function(_,b) return fmt("function %s end", b:sub(2,#b-1)) end) end
- 
-function Hint(s)
-  return s:gsub("(function[^\n]+)(%b())(%s*:[^\n]*)\n",
-    function(a,b,c) return fmt("%s%s\n",a,b:gsub(":[^,)]+","")) end) end
 
-function From(s,        act)
-  function act (file,wants,     sep,pre,post) 
-    sep,pre,post = "","",""
-    for want in wants:gmatch("([^ ]+)") do 
-      pre  = pre  .. sep .. want
-      post = post .. sep .. fmt('require("%s").%s',file,want) 
-      sep  = ", " end
-    return fmt("\nlocal %s = %s\n",pre,post) 
-  end
-  return s:gsub("\nfrom%s+([^\n]*)%s+import([^\n]*)\n", act) end
+local seen={}
+function inquire(path,    file,content)
+  if   seen[path] 
+  then return seen[path]
+  else file = io.input(path)
+       content = file:read "*a" 
+       io.close(file)
+       seen[path] = load(lunatic(content))()
+       return seen[path] end end
 
 -- return lunatic
-----------------------------------------------
-
-local s = lunatic[[
+local function demo()
+ return print(lunatic[[
 
 from f1 import aa mm nn
 
@@ -50,49 +74,16 @@ function asda(a : ?[int],b:num) : fasdas
 
 ^aa
 
-`((x) a (1 1+3 ^(aa)))
+map(x,`((_,y) 
 
-`((y) ^a)
-]]
+^x+1))
 
-print(s)
+]]) end
 
+demo()
 ----------------------------------------------
 for k,v in pairs(_ENV) do if not b4[k] then print("?? rogue",k,type(v)) end end 
-  
-function collect(t,f,walk,  u) 
-  walk = walk or pairs
-  u={}
-  for k,x in walk(t) do 
-    x,also = f(k,x) 
-    if   also == nil 
-    then if x~= nil then u[#u+1] = x end
-    else if x~= nil then u[x] = also end end end
-  return u end
 
--- s=s:gsub("[(](.*)for.*(%w).*in(.*)[)]",five)
--- s=s:gsub("[(](.*)for.*(%w).*on(.*)[)]",five)
-  
--- s=s:gsub("[(](.*)for.*(%w)%s*,%s*(%w).*in(.*)[)]",five)
--- s=s:gsub("[(](.*)for.*(%w)%s*,%s*(%w).*on(.*)[)]",five)
 
-function Comprehension(s,   act)
-  function collect(a,b,c,d,e)
-    return fmt("collect(%s,function (%s,%s) return %s end),%s)",
-               a,b,c,d,e) end
-  function pairs1(src,x,t)    return collect(t,"_",x,src,"pairs") end
-  function pairs2(src,k,x,t)  return collect(t,k,   x,src,"pairs") end
-  function ipairs1(src,x,t)   return collect(t,"_",x,src,"ipairs") end
-  function ipairs2(src,k,x,t) return collect(t,k,  x,src,"ipairs") end
-
-  s= s:gsub("`[(](.*)for%s+(%S+)%s*,%s*(%S+)%s+in(.*)[)]", ipairs2) 
-  s= s:gsub("`[{](.*)for%s+(%S+)%s*,%s*(%S+)%s+in(.*)[}]",  pairs2) 
-  s= s:gsub("`[(](.*)for%s+(%S+)%s+in(.*)[)]", ipairs1) 
-  s= s:gsub("`[{](.*)for%s+(%S+)%s+in(.*)[}]",  pairs1) 
-  return s end
-
-s=Comprehension("`(assa() and b(aa) for aa , bb in sds(aa))"); print(100,s)
-s=Comprehension("`{assa() and b(aa) for aa , bb in sds(aa)}"); print(200,s)
-s=Comprehension("`{assa() and b(aa)  for    aa   in sds })");  print(300,s)
-s=Comprehension("`{assa() and b(aa)  for    aa   in sds })");  print(400,s)
+return inquire
 
