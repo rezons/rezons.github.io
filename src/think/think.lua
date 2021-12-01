e47e6842417799b4dd93429a0a1018715c820a4e
Tim Menzies <timm@users.noreply.github.com>
2021-12-01 18:24:21 -0500

    save

--- src/think/think.lua
+++ src/think/think.lua
@@ -1,13 +1,38 @@
-local b4={}; for k,v in pairs(_ENV) do b4[k]=v end
+-------------------------------------------------------------------------------
+-- options
+local the={trivial=.35, small=.5, p=2,b4={}}
+for k,v in pairs(_ENV) do the.b4[k]=v end
 
 -------------------------------------------------------------------------------
-local the={p=2}
-
--------------------------------------------------------------------------------
+-- maths tricks
 local abs
 abs = math.abs
 
+function norm(x,lo,hi)
+  if x=="?" then return x end
+  return abs(hi - o) < 1E32 and 0 or (x - lo)/(hi - lo) end
+
+function per(t,p,    here)
+  function here(x) x=x*#t//1; return x < 1 and 1 or x>#t and #t or x end
+  return #t <2 and  t[1] or t[ here(p or .5) ] end
+
+function sd(t) return (per(t,.9) - per(t,.1))/ 2.56 end
+
+function xpect(t1,t2) return (#t1*sd(t1) + #t2*sd(t2)) / (#t1 + #t2) end
+
+function mergeable(t1,t2,    t3)  
+  t3 = {}
+  for _,x in pairs(t1) do push(t3,x) end
+  for _,x in pairs(t2) do push(t3,x) end
+  t3 = sort(t3) 
+  if xpect(t1,t2) >= sd(t3) then return t3 end end
+
+function sum(t,f)
+  f= f or function(x) return x end
+  out=0; for _,x in pairs(f) do out = out + f(x) end; return out end
+
 -------------------------------------------------------------------------------
+-- table tricks
 local cat,map,push,sort,firsts,shuffle
 cat     = table.concat
 sort    = function(t,f) table.sort(t,f); return t end
@@ -23,6 +48,7 @@ function map(t,f,     u)
   return u end
 
 -------------------------------------------------------------------------------
+-- printing tricks
 local fmt,out,shout
 fmt  = string.format
 shout= function(x) print(out(x)) end
@@ -41,6 +67,7 @@ function out(t,    u,key,keys,value,public)
   return (t._is or"").."{"..cat(u," ").."}" end 
 
 -------------------------------------------------------------------------------
+-- file i/o tricks
 local lines
 function lines(file,fun,    line,t,out)
   fun  = fun or function(x) return(x) end
@@ -58,77 +85,131 @@ function lines(file,fun,    line,t,out)
   return  out end
 
 -------------------------------------------------------------------------------
+-- oo tricks
 local has,obj
 function has(mt,x) return setmetatable(x,mt) end
 function obj(s, o,new)
    o = {_is=s, __tostring=out}
    o.__index = o
-   new = function(_,...) return o.new(...) end
-   return setmetatable(o,{__call=new}) end
+   return setmetatable(o,{__call = function(_,...) return o.new(...) end}) end
 
 -------------------------------------------------------------------------------
+-- Samples store rows. They know about 
+-- (a) lo,hi ranges on the numerics
+-- and (b) what  are independent `x` or dependent `y` columns.
 local Sample=obj"Sample"
-function Sample.new(      file,self) 
-  self= has(Sample,{head=nil, nums={}, ys={}, xs={}, rows={}}) 
-  return file and self:load(file) or self end
-
-function Sample:load(file)
-  for _,row in pairs(lines(file)) do self:add(row) end 
-  return self end
+function Sample.new()
+  return has(Sample,{names=nil, nums={}, ys={}, xs={}, rows={}})  end
 
 function Sample:add(row,     headers,data)
-  function headers(at,head,    tmp) 
-    if not head:find":" then
-      if head:match("^[A-Z]") then 
-        tmp = {at=at, w=0, lo=1E32, hi=-1E22} 
-        if head:find"-" then tmp.w=-1; self.ys[at] = tmp end
-        if head:find"+" then tmp.w= 1; self.ys[at] = tmp end
-        self.nums[at] = tmp end
-      if not (head:find("+") or head:find("-")) then self.xs[at]=at end end
-    return head 
-  end
-  function data(at,datum)
-    if self.nums[at] and datum ~= "?" then
-      self.nums[at].lo = math.min(datum, self.nums[at].lo)
-      self.nums[at].hi = math.max(datum,  self.nums[at].hi) end 
-    return datum 
-  end
-  if   self.head
-  then push(self.rows, map(row,data)) 
-  else self.head = map(row,headers) end end 
+  function name(col,new,    tmp) 
+    if not new:find":" then return end
+    if not (new:find("+") or new:find("-")) then self.xs[col]=col end 
+    if new:match("^[A-Z]") then 
+      tmp = {col=col, w=0, lo=1E32, hi=-1E22} 
+      self.nums[col] = tmp
+      if new:find"-" then tmp.w=-1; self.ys[col] = tmp end
+      if new:find"+" then tmp.w= 1; self.ys[col] = tmp end end 
+  end -----------------
+  function datum(col,new)
+    if self.nums[col] and new ~= "?" then
+      self.nums[col].lo = math.min(new, self.nums[col].lo)
+      self.nums[col].hi = math.max(new, self.nums[col].hi) end  
+  end -----------------
+  if   not self.names
+  then self.names = row
+       map(row, function(col,x) return self:name(col,x) end) 
+  else push(self.rows, row)
+       map(row, function(col,x) return self:datum(col,x) end) end end
 
-function Sample:norm(num,x)
-  if x=="?" then return x end
-  return abs(num.hi - num.lo) < 1E32 and 0 or (x - num.lo)/(num.hi - num.lo) end
-
-function Sample:better(row1,row2,      e,n,a,b,s1,s2)
-  n,s1,s2,e = #self.ys, 0, 0, 2.71828
-  for _,num in pairs(self.ys) do
-    a  = self:norm(num, row1[num.at])
-    b  = self:norm(num, row2[num.at])
-    s1 = s1 - e^(num.w * (a-b)/n) 
-    s2 = s2 - e^(num.w * (b-a)/n) end
-  return s1/n < s2/n end 
-
-function Sample:dist(row1,row2)
-  function dist(num)
+-------------------------------------------------------------------------------
+-- geometry tricks
+-- y column rankings
+local dist, better,betters
+function dist(row1,row2,sample,     a,b,d,n,inc,dist1)
+  function dist1(num)
     if not num then return a==b and 0 or 1 end
-    if     a=="?" then b=self:norm(num,b); a = b>.5 and 0 or 1
-    elseif b=="?" then a=self:norm(num,a); b = a>.5 and 0 or 1
-    else   a,b = self:norm(num,a), self:norm(num,b) 
+    if     a=="?" then b=norm(b, num.lo, num,hi); a = b>.5 and 0 or 1
+    elseif b=="?" then a=norm(a, num.lo, num.hi); b = a>.5 and 0 or 1
+    else   a,b = norm(a, num.lo, num.hi), norm(b, num.lo, num.hi)
     end
     return abs(a-b) 
   end -------------------------
-  for at,_ in pairs(self.xs) do
-    a,b = row1[at], row2[at]
-    inc = a=="?" and b=="?" and 1 or dist(self.nums[at])
+  d,n=0,0
+  for col,_ in pairs(sample.xs) do
+    a,b = row1[col], row2[col]
+    inc = a=="?" and b=="?" and 1 or dist1(sample.nums[col])
     d   = d + inc^the.p
     n   = n + 1 end
   return (d/n)^(1/the.p) end
 
-s=Sample.new("../../data/auto93.csv")
+function betters(rows,sample) 
+  return sort(rows,function(a,b) return better(a,b,sample) end) end
+
+function better(row1,row2,sample,     e,n,a,b,s1,s2)
+  n,s1,s2,e = #sample.ys, 0, 0, 2.71828
+  for _,num in pairs(sample.ys) do
+    a  = norm(row1[num.col], num.lo, num.hi)
+    b  = norm(row2[num.col], num.lo, num.hi)
+    s1 = s1 - e^(num.w * (a-b)/n) 
+    s2 = s2 - e^(num.w * (b-a)/n) end
+  return s1/n < s2/n end 
+
+function bins(col,sample,      xys, x,y,tmp,out,n,xpect)
+  xys,all, symbols, symbolic ={},{}, {}, sample.nums[col]
+  for rank,row in pairs(sample.rows) do
+    x,y = row[col], rank
+    if x ~= "?" then 
+      if   symbolic
+      then symbols[x] = symbols[x] or {}
+           push(tmp[x], y) 
+      else push(all,x)
+           push(xys,{x=x,y=y}) end end end
+  if   symbolic 
+  then out = map(symbols, function(x,t) return {col=col, lo=x, hi=x, has=sort(t)} end)
+  else xys = sort(xys, function(a,b) return a.x < b.x end)
+       out = discretize(col,xys, #all^the.small,  sd(sort(all)) * the.trivial) 
+  end
+  xpect = sum(out, function(one) return #one.has/#all*sd(one.has) end)
+  return {xpect, out} end
+
+-- Generate a new range when     
+-- 1. there is enough left for at least one more range; and     
+-- 2. the lo,hi delta in current range is not tiny; and    
+-- 3. there are enough x values in this range; and   
+-- 4. there is natural split here
+-- Fuse adjacent ranges when:
+-- 5. the combined class distribution of two adjacent ranges 
+--    is just as simple as the parts.
+function discretize(col, xys, tiny, dull,           now,out,x,y)
+  now = {col=col, lo=xys[1].x, hi=xys[1].x, has={}}
+  out = {now}
+  for j,xy in pairs(xys) do
+    x, y = xy.x, xy.y
+    if   j<#xys-tiny and x~=xys[j+1].x and #now.has>tiny and now.hi-now.lo>dull 
+    then now = {col=col, lo=x, hi=x, has={}}
+         push(out, now) end 
+    now.hi = x 
+    push(now.has, y) end
+  return merge(col, map(out, function(_,one) table.sort(one.has) end)) end 
+
+function merge(col, b4,       j,tmp,a,n,hasnew) 
+  j, n, tmp = 0, #b4, {}
+  while j<n do
+    j = j + 1
+    a = b4[j]
+    if j < n-1 then
+      better = mergeable(a.has, b4[j+1].has)
+      if better then 
+        j = j + 1 
+        a = {col=col, lo=a.lo, hi= b4[j+1].hi, has=better} end end
+    push(tmp,a) end 
+  return #tmp==#b4 and b4 or merge(col,tmp) end
+
+s=Sample.new()
+for _,row in pairs(lines("../../data/auto93.csv")) do s:add(row) end
 
 for _,row in sort(s.rows, function(a,b) return s:better(a,b) end)  do
    shout(row) end
 
-for k,v in pairs(_ENV) do if not b4[k] then print("? ",k,type(v)) end end
+for k,v in pairs(_ENV) do if not the.b4[k] then print("? ",k,type(v)) end end
