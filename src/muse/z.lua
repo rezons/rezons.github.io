7dc9673acdb3418b1eb0743153f6ade375651281
Tim Menzies <timm@ieee.org>
2021-12-27 00:01:18 -0500

    save

--- src/muse/z.lua
+++ src/muse/z.lua
@@ -1,17 +1,19 @@
 local b4={}; for k,_ in pairs(_ENV) do b4[k]=k end
-local btw,hue,main,cli
+local btw,hue,keys,main,cli
 
 function btw(...) io.stderr:write(string.format(...).."\n") end
 function hue(n,s) return string.format("\27[1m\27[%sm%s\27[0m",n,s) end
+function keys(t,   u) 
+  u={}; for k,_ in pairs(t) do u[1+#u]=k ; print(k) end; table.sort(u); return u end
 
 function main(globals,acts,     fails,defaults,todos,ok,msg)
-  fails=0
+  fails, defaults = 0,{}
   for k,v in pairs(globals) do defaults[k]=v end
-  if globals.debug then act[ globals.debug ]() end
-  todos = THE.todo == "all" and keys(go) or THE.todo and {THE.todo} or {}
+  if globals.debug then acts[ globals.debug ]() end
+  todos= globals.todo=="all" and keys(acts) or globals.todo and {globals.todo} or {}
   for _,todo in pairs(todos) do
      for k,v in pairs(defaults) do globals[k]=v end
-     ok,msg = pcall( go[todo] )
+     ok,msg = pcall( acts[todo] )
      if ok then btw("%s%s",hue(32,"-- PASS "),todo)
          else btw("%s%s %s",hue(31,"-- FAIL "),todo,msg); fails=fails+1 end 
   end
@@ -20,7 +22,7 @@ function main(globals,acts,     fails,defaults,todos,ok,msg)
     if not b4[k] then btw(hue(31,"-- rogue? %s %s"),k,type(v)) end end
   os.exit(fails) end
 
-function cli(txt)
+function cli(txt,    out)
   out={}
   txt:gsub("\n  [-]([^%s]+)[^\n]*%s([^%s]+)",
     function(flag,x)
@@ -32,7 +34,7 @@ function cli(txt)
       if x=="true" then x=true elseif x=="false" then x=false else x=tonumber(x) or x end
       out[flag] = x end)
   out.seed = out.seed or 10019
-  if out.h then return print(txt) end 
-  return out end
+  if out.h then print(txt) end 
+  return setmetatable(out, {__call= main}) end
 
-return {cli=cli, main=main}
+return cli
