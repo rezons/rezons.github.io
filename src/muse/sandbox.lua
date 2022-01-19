5db90172119c8dbd90b239b60f9995e98fe8fd88
Tim Menzies <timm@ieee.org>
2022-01-19 04:12:01 -0500

    save

--- src/muse/sandbox.lua
+++ src/muse/sandbox.lua
@@ -1,27 +1,52 @@
-local map,words,o,is,klass
-function map(t,f,   u) u={};for _,v in pairs(t)  do u[1+#u]=f(v) end; return u end
-function words(s,z, u) u={};for w in s:gmatch(z) do u[1+#u]=w    end; return u end
+local the,help,cat, fmt,map,words,o,is,klass = {},[[
+aa
+(C)2022 tim menzies
+
+  -h  false
+]]
+
+cat = table.concat
+fmt = string.format
+as  = setmetatable
+function klass(is,public,    t,new)
+  t={}; t.__index=t
+  function new(_,...)      return t.new(...) end
+  function t.__tostring(i) return is..o(i,public) end
+  return as(t,{__call=new}) end
+
+function map(t,f, u) u={};for _,v in pairs(t)    do u[1+#u]=f(v) end; return u end
+function words(s, u) u={};for w in s:gmatch"([^,]+)" do u[1+#u]=w end;return u end
+function atom(x)   
+  if x=="true" then return true elseif x=="false" then return false end
+  return tonumber(x) or x end
+
+function csv(file)
+  file = io.input(file) 
+  return function(    t) 
+    x=io.read(); if x then return map(words(x:gsub("%s+","")),atom) end 
+    io.close(file) end end 
 
 function o(t,s)
-  local f = function(slot) return string.format(":%s %s",slot,t[slot]) end
-  local u = s and map(words(s,"([^,]+)"),f) or map(t,tostring)
-  return "{"..table.concat(u," ").."}" end
+  local f = function(slot) return fmt(":%s %s",slot,t[slot]) end
+  return "{"..cat(s and map(words(s,"([^,]+)"),f) or map(t,tostring)," ").."}" end
 
-is=setmetatable
-function klass(    t,new)
-  function new(_,...) return t.new(...) end
-  t={}; t.__index = t; return is(t,{__call=new}) end
-
-Pet=klass("Pet","age name")
-function Pet.__add(i,j)    return i.age+j.age end
-function Pet.__tostring(i) return "Pet"..o(i,"age,name") end
-function Pet.new(name)     return is({age=10,name=name or "Fido"},Pet) end
+------------------------------------------
+Pet=klass("Pet","age,name")
+function Pet.new(name)     return as({age=10,name=name or "Fido"},Pet) end
 function Pet.fred(i)       print(i.name) end
+
+------------------------------------------
+help:gsub("\n  [-]([^%s]+)[^\n]*%s([^%s]+)", function(flag, x)
+  for n,txt in ipairs(arg) do             
+    if   txt:sub(1,1)=="-" and flag:match("^"..txt:sub(2)..".*") 
+    then x = x=="false" and"true" or x=="true" and"false" or arg[n+1] end end 
+  the[flag] = atom(x) end)
+
+if the.h then print(help) end
 
 x=Pet("loeverboy")
 y=Pet()
 y.age =10000
-print(x+y)
 print(x)
 x:fred()
 print(o{10,20,30})
