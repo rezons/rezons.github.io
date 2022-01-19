261b2e4c23f435cbd0fb5a0bc9b18daf6449383f
Tim Menzies <timm@ieee.org>
2022-01-19 02:03:21 -0500

    save

--- src/muse/sandbox.lua
+++ src/muse/sandbox.lua
@@ -1,22 +1,27 @@
-isa=setmetatable
-fmt=string.format
-function o(t,txt,    s,sep)
-  sep=""
-  if txt then for w in txt:gmatch("%S+") do 
-    if not s then s=w.."{" else s=fmt("%s%s:%s %s",s,sep,w,t[w]);sep=" " end end
-  else for _,v in pairs(t) do 
-    if not s then s="{"..v else s=fmt("%s%s%s",s,sep,v);sep=" "end end end
-  return s .."}" end
-  
+local map,words,o,is,klass
+function map(t,f,   u) u={};for _,v in pairs(t)  do u[1+#u]=f(v) end; return u end
+function words(s,z, u) u={};for w in s:gmatch(z) do u[1+#u]=w    end; return u end
 
-Pet={}
+function o(t,s)
+  local f = function(slot) return string.format(":%s %s",slot,t[slot]) end
+  local u = s and map(words(s,"([^,]+)"),f) or map(t,tostring)
+  return "{"..table.concat(u," ").."}" end
+
+is=setmetatable
+function klass(    t,new)
+  function new(_,...) return t.new(...) end
+  t={}; t.__index = t; return is(t,{__call=new}) end
+
+Pet=klass("Pet","age name")
 function Pet.__add(i,j)    return i.age+j.age end
-function Pet.__tostring(i) return o(i,"Pet age") end
-function Pet.new()         return isa({age=10},Pet) end
+function Pet.__tostring(i) return "Pet"..o(i,"age,name") end
+function Pet.new(name)     return is({age=10,name=name or "Fido"},Pet) end
+function Pet.fred(i)       print(i.name) end
 
-x=Pet.new()
-y=Pet.new()
+x=Pet("loeverboy")
+y=Pet()
 y.age =10000
 print(x+y)
 print(x)
-
+x:fred()
+print(o{10,20,30})
